e5e128845782a80cf64e075419a1a27348490cd8
jmemich <jamesgmichelson@gmail.com>
2014-05-20 02:31:50 -0400

    first commit

---
+++ raw_text_data_clean.R
@@ -0,0 +1,36 @@
+## cleaning raw Luke Hilliman network data from :http://www.lukehillman.net/2011/06/social-network-analysis/
+## for ICPSR adv. network analysis class
+
+# get text file with raw data
+raw_data <- readLines("C:/Users/Jamie/Documents/GitHub/icpsr-adv_networks-lotr-data/luke_hilliman-raw_lotr_data.txt") 
+
+# unique names
+unique_names <- unlist(unique(lapply(strsplit(grep("^[^0-9]", raw_data, value = TRUE), " and "), function(X) unique(X[[1]]))))
+
+# create edgelist
+edgelist <- data.frame(expand.grid(unique_names, unique_names), stringsAsFactors = FALSE)[, 2:1] ## reverse col order (prettier!)
+edgelist$edge <- 0
+	
+# iterate over each pair of characters
+# add edge if any one meeting across all three books!
+for(i in unique_names)  {
+
+	for(ii in unique_names) {
+
+		line_number <- grep(paste(i, "and", ii), raw_data)
+
+		if(length(line_number) == 0) {cat(print("fuckup here:", line_number))}
+
+		# if any of the next 3 lines (1 line for each book) ends in '1' then the two character meet
+		if(any(grepl("[1-9]$", raw_data[c(line_number + 1, line_number + 2, line_number + 3)]))) {
+
+			edgelist$edge[edgelist[, 1] == i & edgelist[, 2] == ii] <- 1
+		
+		}
+
+	}
+
+}
+
+# remove any lines in edgelist without ties
+edgelist <- edgelist[edgelist$edge == 1, ]
